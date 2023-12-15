32d005229ce2e2c9bf6471f08625ab361f38832c
Zi-Shen <zi-shen@outlook.com>
2023-12-15 22:45:15 +0800

    update start from the position last edit

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def find_name_in_grade_book(name):
     print(f'Cannot find {name} in the grade book.')
     return None
 
+counter_graded = 0
 for name_info in filelist:
     temp_path = path + "/" + name_info + "/"
     path_pdf = temp_path + os.listdir(temp_path)[0]
@@ -30,19 +31,30 @@ for name_info in filelist:
     name = name_info.split('_')[0]
     
     row = find_name_in_grade_book(name)
+    counter_graded += 1
+
+    if sheet.cell(row, 16).value is not None or sheet.cell(row, 9).value is not None or sheet.cell(row, 13).value == 0:
+        if sheet.cell(row, 13).value == 0 or sheet.cell(row, 13).value == '0':
+            print(f'{name} has not submitted the assignment before 2 day after deadline. Skip grading...')
+            continue
+        continue
+
+    print(f'{counter_graded}/{len(filelist)} has been graded, good job!')
+    print(f'Start grading {name}...')
 
     subprocess.run(f'start msedge file:///{path_pdf}', shell=True)
 
     # input the grade and feedback
-    grade = input("Input the grade:\n")
     feedback = input("Input the feedback:\n")
+    grade = input("Input the grade:\n")
 
     # write the grade and feedback to the file
 
-    sheet.cell(row, 9).value = grade
+    sheet.cell(row, 9).value = float(grade)
     sheet.cell(row, 16).value = feedback
     workbook.save(grade_book_file)
     counter += 1
+    print(f'{name} has been graded.\n')
 
     # kill the msedge process every 6 assignments
     if counter % 6 == 0:
